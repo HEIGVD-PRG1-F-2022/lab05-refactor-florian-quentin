bf2a6e47672d7e97ee800b9de7419be18b168a62
SuperQuentin <quentin.aellen@infrack.ch>
2022-11-16 17:07:14 +0100

    feat: add cursor, color and set content implementation

--- include/display.h
+++ include/display.h
@@ -11,15 +11,32 @@
 #include <string>
 
 class Display {
+    enum Colors {
+        WHITE = 15,
+        PINK = 207,
+        RED = 196,
+        GREEN = 40,
+        BLUE = 21,
+        BLACK = 16
+    };
+
     std::string prefix;
     std::string suffix;
+
     std::string content;
 public:
-    Display();
+    explicit Display(Colors defaultTextColor = Colors::WHITE);
+
+    void setTextColor(Colors color);
+
     void saveCursorPosition();
+
     void goBackToCursorPosition();
-    void show(bool overwrite_prev = false);
+
+    void show();
+
     void setContent(std::string content);
+
     void clear();
 };
 
--- src/display.cpp
+++ src/display.cpp
@@ -8,10 +8,27 @@
 #include <iostream>
 #include "../include/display.h"
 
-void Display::saveCursorPosition() {
-
+Display::Display(Colors defaultTextColor) {
+    this->setTextColor(defaultTextColor);
 }
 
-void Display::show(bool overwrite_prev) {
-    std::cout << "\033[s" <<  this->content << std::endl;
+void Display::setTextColor(Display::Colors color) {
+    this->prefix = "\x1b[38;5;" + std::to_string(color) +  "m";
+    this->suffix = "\x1b[0m";
+}
+
+void Display::saveCursorPosition() {
+    std::cout << "\033[s";
+}
+
+void Display::goBackToCursorPosition() {
+    std::cout << "\033[u";
+}
+
+void Display::show() {
+    std::cout << this->prefix << this->content << this->suffix << std::endl;
+}
+
+void Display::setContent(std::string content) {
+    this->content = content;
 }
